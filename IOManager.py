864fc5adb786f3592be6e21f721aa6d120b41cb2
Dugowtich <jakubdugovic@gmail.com>
2023-02-27 10:55:02 +0100

    fix: reading input, escape sequence parsing, _getOps, type conversions, stack methods

--- IOManager.py
+++ IOManager.py
@@ -1,18 +1,28 @@
 import sys
+import re
 
 class IOManager:
     def __init__(self, ARGS):
-        self.INPUT = []
+        self.INPUT = None
         if ARGS.input:
-            with open(ARGS.input) as f:
-                raw = f.read()
-                self.INPUT = raw.split("\n")
-        else:
-            self.INPUT = self.read().split("\n")
-            self.INPUT.reverse()
+            self.INPUT = open(ARGS.input, "r")
+
+    def __del__(self):
+        if self.INPUT:
+            self.INPUT.close()
 
     def readOne(self):
-        return self.INPUT.pop()
+        text = None
+        try:
+            if self.INPUT:
+                text = self.INPUT.readline().strip()
+            else:
+                text = input()
+        except EOFError:
+            pass
+        if (text == ""):
+            text = None
+        return text
 
     def read(self):
         return sys.stdin.read()
@@ -21,5 +31,14 @@ class IOManager:
         if isError:
             print(message, file=sys.stderr, end="")
         else:
-            # print(message, end="") # TODO: replace the line below by this
+            if isinstance(message, str):
+                matches = re.findall(r"\\[0-9]{3}", message)
+                for match in matches:
+                    code = re.sub(r"\\0*", "", match)
+                    char = chr(int(code))
+                    # print(f"> \'{match}\' {type(match)} -> \'{char}\' {type(char)}") # TODO: remove
+                    message = message.replace(match, char)
+            elif message == None:
+                message = ""
             print(message)
+            # print(message, end="") # TODO: replace the line above by this
